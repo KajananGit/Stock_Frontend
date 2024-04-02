88464704af1977f4416b55b13d233d69d7631a95
Kajanan <Kajanan15i@gmail.com>
2024-04-02 14:09:15 +0100

    updated boilerplates

--- stock_frontend/src/App.js
+++ stock_frontend/src/App.js
@@ -3,6 +3,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import OrdersContainer from './containers/OrdersContainer';
 import HomepageContainer from './containers/HomepageContainer';
+import Navigation from './components/Navigation';
 import OrdersList from './components/OrdersList';
 import NewOrderForm from './components/NewOrderForm';
 import NewOrderedItemForm from './components/NewOrderedItemForm';
@@ -12,8 +13,12 @@ function App() {
 
   const warehouseRoutes = createBrowserRouter([
     {
+      path: "/home",
+      element:<HomepageContainer />
+    },
+    {
       path: "/",
-      element: <HomepageContainer />,
+      element: <Navigation />,
       children: [
         {
           path: "/orders",
--- stock_frontend/src/containers/OrdersContainer.js
+++ stock_frontend/src/containers/OrdersContainer.js
@@ -3,9 +3,23 @@ import Navigation from "../components/Navigation";
 import OrderSearch from "../components/OrderSearch";
 import NewOrderedItemForm from "../components/NewOrderedItemForm";
 import NewOrderForm from "../components/NewOrderForm";
+import { useEffect, useState } from "react";
 
 const OrdersContainer = () => {
    
+    const [stockId, setStockId] = useState(null);
+
+    const loadStocksData = async () => {
+        const response = await fetch("http://localhost:8080/stocks");
+        const jsonData = await response.json();
+        console.log(jsonData);
+    }
+
+    useEffect(() => {
+        loadStocksData();
+    }, []);
+
+
     
     return (
         <>
